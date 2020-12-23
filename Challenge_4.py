5ea5d25f12ec85bbf0d4d922bb4fb67fd1be6ca5
Noah Woodin <59379626+noahwoodin@users.noreply.github.com>
2020-12-22 20:51:08 -0800

    -First attempt at challenge 4

---
+++ Challenge_4.py
@@ -0,0 +1,17 @@
+# Coding Challenge #4 (Hard)
+
+# Given an array of integers, find the first missing positive integer in linear time and constant space.
+# In other words, find the lowest positive integer that does not exist in the array.
+# The array can contain duplicates and negative numbers as well.
+
+def smallest_pos(array):
+    array = [num for num in array if num > 0]
+    array.sort()
+    for counter, num in enumerate(array):
+        if counter+1 != num:
+            return counter+1
+    return counter+2
+
+
+print(smallest_pos([3, 4, -1, 1]))
+print(smallest_pos([1, 2, 0]))
